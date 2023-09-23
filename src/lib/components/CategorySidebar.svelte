5f1f407cd76caa90041c51612188b6ac64fbac7d
gantcho.kojuharov <gantcho.kojuharov@rws.nl>
2023-09-23 14:59:34 +0200

    initial

---
+++ src/lib/components/CategorySidebar.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let categories = [];
+</script>
+
+<div class="w-32 mr-8 border-r pr-3">
+    <ul>
+        {#each categories as category}
+        <li class="mb-8">
+            <span class="text-white hover:underline cursor-pointer">{category}</span>
+        </li>
+        {/each}
+    </ul>
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	// The ordering of these imports is critical to your app working properly
+	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+	// If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// Most of your app wide CSS should be put in this file
+	import '../app.postcss';
+
+	import { AppShell } from '@skeletonlabs/skeleton';
+</script>
+
+<AppShell>
+	<!-- <svelte:fragment slot="header">Header</svelte:fragment> -->
+	<!-- <svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment> -->
+	<!-- (sidebarRight) -->
+	<!-- (pageHeader) -->
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+	<!-- <svelte:fragment slot="pageFooter">Page Footer</svelte:fragment> -->
+	<!-- (footer) -->
+</AppShell>
