2bdad30e7c2448beb48a74234a59c822e0db4b60
Gantcho Kojuharov <gantcho@greyarch.net>
2023-09-24 16:27:59 +0200

    added a login page (html only)

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,13 @@
+<!-- component -->
+<div class="mx-auto my-36 flex h-[300px] w-[350px] flex-col border-2 bg-white text-black shadow-xl">
+	<div class="mx-8 mt-7 mb-1 flex flex-row justify-start space-x-2">
+		<div class="w-3 text-center font-sans text-xl font-bold"><h1>Login</h1></div>
+	</div>
+	<div class="flex flex-col items-center">
+		<input class="my-2 w-72 border p-2" type="email" placeholder="Username" />
+		<input class="my-2 w-72 border p-2" type="password" placeholder="Password" />
+	</div>
+	<div class="my-2 flex justify-center">
+		<button class="w-72 border rounded-md bg-primary-500 p-2 font-sans">Login</button>
+	</div>
+</div>
