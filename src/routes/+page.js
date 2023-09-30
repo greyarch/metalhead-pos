ec5d6b8430bc2238357bce94cc60abf4dff074f9
Gantcho Kojuharov <gantcho@greyarch.net>
2023-09-30 17:26:11 +0200

    removed redundant log

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,16 +1,14 @@
-
 import { authUser } from '$lib/stores/auth';
 import { redirect } from '@sveltejs/kit';
 
 export const load = async () => {
-    const unsubscribe = authUser.subscribe((user) => {
-        console.log('user', user)
-        if (!user) {
-            throw redirect(302, '/login');
-        }
-    });
+	const unsubscribe = authUser.subscribe((user) => {
+		if (!user) {
+			throw redirect(302, '/login');
+		}
+	});
 
-    unsubscribe();
+	unsubscribe();
 
-    return {};
+	return {};
 };
