0f9597c7679b5b842426bf605f72a8948f6ac8a7
Gantcho Kojuharov <gantcho@greyarch.net>
2023-10-28 22:04:39 +0200

    do not render server side

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,6 +1,9 @@
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from '$env/static/public';
 import { createSupabaseLoadClient } from '@supabase/auth-helpers-sveltekit';
 
+export const ssr = false;
+export const csr = true;
+
 export const load = async ({ fetch, data, depends }) => {
 	depends('supabase:auth');
 
