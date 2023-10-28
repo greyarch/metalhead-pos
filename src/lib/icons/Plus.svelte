e39d145604faf740f66cf71e47071f55849ac2bf
Gantcho Kojuharov <gantcho@greyarch.net>
2023-10-28 21:28:58 +0200

    added a plus icon; reformatting

--- src/lib/icons/Plus.svelte
+++ src/lib/icons/Plus.svelte
@@ -5,7 +5,6 @@
 	stroke-width="1.5"
 	stroke="currentColor"
 	class="w-6 h-6"
-	on:click
 >
-	<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+	<path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 </svg>
