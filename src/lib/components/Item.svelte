90bfc2501bdf22acd225afb8a81c35b7bafa1d4e
gantcho.kojuharov <gantcho.kojuharov@rws.nl>
2023-12-20 18:10:00 +0100

    all prices in buttons

--- src/lib/components/Item.svelte
+++ src/lib/components/Item.svelte
@@ -1,24 +1,24 @@
 <script>
 	export let item;
 	export let handleClick;
+
+	function btn(variant) {
+		if (variant.name === 'default') {
+			return `${variant.price}лв.`;
+		}
+		return `${variant.name} - ${variant.price}лв.`;
+	}
 </script>
 
-{#if item.variants?.length === 1}
-	<button
-		class="text-xl font-semibold mr-2 p-1 rounded-md border border-gray-300 hover:bg-gray-100"
-		on:click={handleClick(item, item.variants[0])}
-	>
-		{item.name} {item.variants[0].name === 'default' ? '' : ` (${item.variants[0].name})`} - {item.variants[0].price}лв.
-	</button>
-{:else}
+{#if item.variants?.length}
 	<h2 class="text-xl mb-2 font-semibold float-left w-7/12">{item.name}</h2>
 	<div>
 		{#each item.variants as variant}
 			<button
-				class="mr-2 p-1 rounded-md border border-gray-300 hover:bg-gray-100"
+				class="mr-2 p-1 rounded-md border border-gray-300 hover:bg-gray-100 w-1/6"
 				on:click={handleClick(item, variant)}
 			>
-				{variant.name !== 'default' ? `${variant.name} - ` : ''}{variant.price}лв.
+				{btn(variant)}
 			</button>
 		{/each}
 	</div>
