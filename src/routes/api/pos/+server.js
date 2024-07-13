b7ad28e27acf824b5676a39131873318660bb795
Gantcho Kojuharov <gantcho@greyarch.net>
2024-07-13 17:04:35 +0300

    handle unauthorized error

--- src/routes/api/pos/+server.js
+++ src/routes/api/pos/+server.js
@@ -1,6 +1,8 @@
 import { PRIVATE_POS_URL, PRIVATE_POS_API_KEY } from '$env/static/private';
 
-export async function POST({ request, fetch }) {
+export async function POST({ request, fetch, locals }) {
+	const session = await locals.getSession();
+	if (!session) return new Response(401);
 	const body = await request.text();
 	try {
 		const res = await fetch(PRIVATE_POS_URL, {
@@ -12,11 +14,9 @@ export async function POST({ request, fetch }) {
 			body
 		});
 		const data = await res.text();
-		return new Response(data)
+		return new Response(data);
 	} catch (e) {
-		return {
-			status: 500,
-			body: e
-		};
+		console.error(e);
+		return new Response(500, e);
 	}
 }
