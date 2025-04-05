99c7584c003f58f45b2004155907b74016b3005b
gantcho.kojuharov <gantcho.kojuharov@rws.nl>
2025-04-05 16:53:07 +0200

    catch setCookies after request

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -5,18 +5,23 @@ export const ssr = false;
 export const csr = true;
 
 export const load = async ({ fetch, data, depends }) => {
-	depends('supabase:auth');
+	try {
+		depends('supabase:auth');
 
-	const supabase = createSupabaseLoadClient({
-		supabaseUrl: env.PUBLIC_SUPABASE_URL,
-		supabaseKey: env.PUBLIC_SUPABASE_ANON_KEY,
-		event: { fetch },
-		serverSession: data.session
-	});
+		const supabase = createSupabaseLoadClient({
+			supabaseUrl: env.PUBLIC_SUPABASE_URL,
+			supabaseKey: env.PUBLIC_SUPABASE_ANON_KEY,
+			event: { fetch },
+			serverSession: data.session
+		});
 
-	const {
-		data: { session }
-	} = await supabase.auth.getSession();
+		const {
+			data: { session }
+		} = await supabase.auth.getSession();
 
-	return { supabase, session };
+		return { supabase, session };
+	} catch (error) {
+		console.error("Exception when getting a session, signing out.", error)
+		await supabase.auth.signOut();
+	}
 };
