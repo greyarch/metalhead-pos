3f7ef77495f94eb5f2b9e986cbb7b2a3e3a5cbc4
gantcho.kojuharov <gantcho.kojuharov@rws.nl>
2025-05-29 11:24:58 +0200

    do not se payload with get

--- src/lib/scan.js
+++ src/lib/scan.js
@@ -35,7 +35,7 @@ async function checkService(method, url, payload, timeout) {
 			headers: {
 				'Content-Type': 'application/json'
 			},
-			body: JSON.stringify(payload),
+			// body: JSON.stringify(payload),
 			signal: controller.signal
 		});
 
